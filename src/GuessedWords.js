850a1106c60a6b2f0148609f6046fc7c591cc545
nikhil.d <nikhil.d@menerva.tech>
2021-12-13 23:19:30 +0530

    Tests and code for non zero words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,18 +2,36 @@ import PropTypes from "prop-types";
 
 function GuessedWords(props) {
   let contents;
-  if(props.guessedWords.length === 0) {
+  if (props.guessedWords.length === 0) {
     contents = (
       <span data-test="guess-instructions">
-      Try to guess the secret word !!
+        Try to guess the secret word !!
       </span>
-    )
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatch}</td>
+        <td></td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
-  return (
-  <div data-test="component-guessed-words">
-    {contents}
-  </div>
-  );
+  return <div data-test="component-guessed-words">{contents}</div>;
 }
 GuessedWords.propTypes = {
   guessedWords: PropTypes.arrayOf(
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -26,6 +26,31 @@ describe("if there are no words guessed", () => {
   });
   test("renders instructions to guess a word", () => {
     const instructions = findByTestAttr(wrapper, "guess-instructions");
-    expect(instructions.find('span').text().length).not.toBe(0);
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {
+  let guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "part", letterMatchCount: 5 },
+  ];
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("renders without errors", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders 'guessed words section'", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test("correct number of guessed words", () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
   });
 });
