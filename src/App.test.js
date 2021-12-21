e14add60b0823990d69671e38035a16d387746b9
nikhil.d <nikhil.d@menerva.tech>
2021-12-21 16:26:09 +0530

    mocks added in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,33 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "./App";
 import { findByTestAttr } from "../test/testUtils";
+
+jest.mock("./actions");
+import { getSecretWord as mockGetSecretWord } from "./actions";
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 test("App component renders without error", () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent).toHaveLength(1);
+});
+
+describe("get secret word", () => {
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+  test("getSecretWord on app mount", () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test("getSecretWord does not run on app update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    wrapper.setProps();
+    
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
